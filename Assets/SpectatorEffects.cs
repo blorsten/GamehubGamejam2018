9fb2b02aad7a2c7feafd0c68133954821a6a9977
Peter Witt <peter.witt.joergensen@gmail.com>
2018-06-03 01:42:34 +0200

    Juice

---
+++ Assets/SpectatorEffects.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class SpectatorEffects : MonoBehaviour
+{
+    public PostProcessingBehaviour post;
+
+    void Update()
+    {
+        if (GameManager.Instance.localPlayer)
+            post.enabled = GameManager.Instance.localPlayer.playerMode == PlayerMode.Spectator;
+    }
+}
