bc116e8c717ac1fcee72f70d5b819677b1a78dff
Peter Witt <peter.witt.joergensen@gmail.com>
2018-06-03 12:54:00 +0200

    main menu juice

---
+++ Assets/MainMenuMoveCam.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuMoveCam : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Vector3.zero);
+        transform.Translate(Vector3.right * Time.deltaTime);
+    }
+}
